c1cfef6a3230ef4ee0b2da3261555011d4a8c3f8
verizhang <verii.zhang@gmail.com>
2021-12-03 20:19:32 +0700

    utility: fix pointer parameter in user_utility.go

--- src/utils/user_utility.go
+++ src/utils/user_utility.go
@@ -10,20 +10,20 @@ import (
 	"time"
 )
 
-func BcryptPassword(password string) string {
-	result, _ := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+func BcryptPassword(password *string) string {
+	result, _ := bcrypt.GenerateFromPassword([]byte(*password), bcrypt.DefaultCost)
 	return string(result)
 }
 
-func ComparePassword(hashedPassword string, password string) bool {
-	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+func ComparePassword(hashedPassword *string, password *string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(*hashedPassword), []byte(*password))
 	if err != nil {
 		return false
 	}
 	return true
 }
 
-func GetToken(user models.User) (string, error) {
+func GetToken(user *models.User) (string, error) {
 	var secretKey = os.Getenv("JWT_SECRET")
 	var expired, _ = strconv.Atoi(os.Getenv("JWT_EXPIRED"))
 
