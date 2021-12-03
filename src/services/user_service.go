59988422515a844610a678f119ab843e60ccdb73
verizhang <verii.zhang@gmail.com>
2021-12-03 20:18:40 +0700

    service: create ChangePassword and move function position

--- src/services/user_service.go
+++ src/services/user_service.go
@@ -15,6 +15,18 @@ type UserService struct {
 	UserRepository repositories.UserRepository
 }
 
+func (UserService *UserService) FindAll(ctx *gin.Context) {
+	var userQuery dtos.UserQueryDto
+	err := ctx.ShouldBindQuery(&userQuery)
+	if err != nil {
+		exceptions.BadRequest(ctx, err.Error())
+		return
+	}
+	var data = UserService.UserRepository.FindAll(&userQuery)
+
+	ctx.JSON(http.StatusOK, data)
+}
+
 func (UserService *UserService) Register(ctx *gin.Context) {
 	var user models.User
 	var userDto dtos.UserDto
@@ -24,13 +36,13 @@ func (UserService *UserService) Register(ctx *gin.Context) {
 		return
 	}
 	user = userDto.ToEntity()
-	user.Password = utils.BcryptPassword(user.Password)
+	user.Password = utils.BcryptPassword(&user.Password)
 	result := UserService.UserRepository.DB.Create(&user)
 	if result.Error != nil {
 		exceptions.BadRequest(ctx, result.Error)
 		return
 	}
-	user.Token, err = utils.GetToken(user)
+	user.Token, err = utils.GetToken(&user)
 
 	ctx.JSON(http.StatusOK, user)
 }
@@ -49,8 +61,8 @@ func (UserService *UserService) Login(ctx *gin.Context) {
 		exceptions.BadRequest(ctx, "User not found")
 		return
 	}
-	if utils.ComparePassword(userModel.Password, loginDto.Password) {
-		userModel.Token, err = utils.GetToken(userModel)
+	if utils.ComparePassword(&userModel.Password, &loginDto.Password) {
+		userModel.Token, err = utils.GetToken(&userModel)
 		ctx.JSON(http.StatusOK, userModel)
 	} else {
 		exceptions.BadRequest(ctx, "Username or password wrong")
@@ -58,14 +70,27 @@ func (UserService *UserService) Login(ctx *gin.Context) {
 	}
 }
 
-func (UserService *UserService) FindAll(ctx *gin.Context) {
-	var userQuery dtos.UserQueryDto
-	err := ctx.ShouldBindQuery(&userQuery)
+func (UserService *UserService) ChangePassword(ctx *gin.Context) {
+	var changePasswordDto dtos.ChangePasswordDto
+	err := ctx.ShouldBindJSON(&changePasswordDto)
 	if err != nil {
 		exceptions.BadRequest(ctx, err.Error())
 		return
 	}
-	var data = UserService.UserRepository.FindAll(userQuery)
 
-	ctx.JSON(http.StatusOK, data)
+	var user models.User
+	if err = UserService.UserRepository.DB.Find(&user, models.User{Username: changePasswordDto.Username}).Error; err != nil {
+		exceptions.BadRequest(ctx, err.Error())
+		return
+	}
+
+	if !utils.ComparePassword(&user.Password, &changePasswordDto.OldPassword) {
+		exceptions.BadRequest(ctx, "The old password was not correct")
+		return
+	}
+
+	user.Password = utils.BcryptPassword(&changePasswordDto.NewPassword)
+	UserService.UserRepository.DB.Save(&user)
+
+	ctx.JSON(http.StatusOK, user)
 }
